f649a2faea4e60194a0b7aeb2af59cd2c4e0bac5
AndreiKeino <andreikeino@gmail.com>
2018-06-04 11:19:49 +0500

    done

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
@@ -12,4 +21,61 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m))
+    {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinverse(m)
+}
+
+print('-----matrix inverse examples:')
+print('-----the matrix:')
+m = matrix(1:4, nrow = 2)
+print(m)
+print('-----the matrix inverse:')
+print(solve(m))
+print('-----multiply matrix on its inverse result:')
+print(m %*% solve(m))
+print('-----test the cacheSolve function:')
+m1 <- makeCacheMatrix(m)
+print(cacheSolve(m1))
+print(cacheSolve(m1))
+
+
+
+# vector example with test
+if(FALSE)
+{
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+    m <- x$getmean()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
+v <- makeVector(1:2)
+print(cachemean(v))
+print(cachemean(v))
 }
